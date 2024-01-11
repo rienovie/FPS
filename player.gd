b68cf0867e7a26f1f7d07db5440211015d977ca1
Vince <fisher.vince@gmail.com>
2024-01-10 20:13:48 -0500

    initial commit

--- player.gd
+++ player.gd
@@ -13,8 +13,6 @@ var currentDampValue = 1.0
 @export var airDamping = 0.125
 @export var jumpVelocity = 6.75
 
-
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -34,14 +32,12 @@ func _physics_process(delta):
 		currentSpeed = sprintSpeed
 	else:
 		currentSpeed = walkSpeed
-	
 
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 		currentDampValue = airDamping
 	else:
 		currentDampValue = 1.0
-
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jumpVelocity
